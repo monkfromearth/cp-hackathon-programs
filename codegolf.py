956c9761a65496fbeb1e4d106db05058deb06d05
Sameer Khan <sameerkhanofficial@gmail.com>
2018-01-08 12:28:11 +0000

    Improved Code for Proper Arrays in Color

--- codegolf.py
+++ codegolf.py
@@ -1,5 +1,5 @@
 """
-colors = {(0,0,0):"black",(1,0,0):"red",(0,1,0):"green",(0,0,1):"blue",(1,1,0):"yellow",(0,1,1):"cyan",(1,0,1):"magenta",(1,1,1):"white"}
+colors = {(0,0,0):"black", (0,0,1):"blue", (0,1,0):"green", (0,1,1):"cyan", (1,0,0):"red", (1,0,1):"magenta",  (1,1,0):"yellow",  (1,1,1):"white"}
 for t in range(int(raw_input().strip())):
     s,r,g,b = map(int, raw_input().strip().split(" "))
     print colors[tuple([((s/i) % 2) for i in [r,g,b]])] 
@@ -7,7 +7,7 @@ for t in range(int(raw_input().strip())):
 ------------------------------------------------------------
 """
 
-c=["black","red","green","blue"",yellow","cyan","magenta","white"]
+c=["black","blue","green","cyan","red","magenta","yellow","white"]
 for t in range(input()):
     s,r,g,b = map(int,raw_input().split())
     print c[int("".join(map(str,[((s/i)%2) for i in [r,g,b]])),2)] 
